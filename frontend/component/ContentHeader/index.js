1d23234692ad52ee1f75632946afb766b9125e6d
Olamide Tosin <larryTosn@gmail.com>
2022-04-11 08:33:27 -0400

    Balance at the top of the page

--- frontend/component/ContentHeader/index.js
+++ frontend/component/ContentHeader/index.js
@@ -1,11 +1,34 @@
 import styles from '../../styles/Home.module.css';
 import * as React from 'react';
 import * as Icon from '@heroicons/react/solid';
+import BASE_URL from '../../Utils';
 
 const ContentHeader = ({progressStatus, progressCompleted}) => {
+        const [loanBalance, setLoanBalance] = React.useState('');
+    const formatValue = (value) => Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'NGN',
+        maximumSignificantDigits: 3,
+      }).format(value);
+
+    React.useEffect(()=>{
+        let mounted = true;
+        if(mounted){
+            axios.get(`${BASE_URL}loan/offer`)
+            .then(res=>{
+                console.log(res.data.data.loans)
+                setLoanBalance(res.data.data.loans[0].loan_amount)
+            })
+            .catch(err=>console.log(err.message))
+        }
+        return ()=> mounted = false;
+    },[]);
     return(
         <div className={styles.contentHeader}>
-            <h2>Payment Option</h2>
+            <div className={styles.titleBalanceContainer}>
+                <h2>Payment Option</h2>
+                {loanBalance && <p>Current Loan: <span>{` ${formatValue(loanBalance)}` || ' You have no pending loan'}</span></p>}
+            </div>
             <div className={styles.progressBar}>
                 <span>{progressStatus ? (progressCompleted ? '3': '2') : '1'} of 3</span>
                 <div className={progressStatus ? (progressCompleted ? styles.progressCircleComplete: styles.progressCircle2) : styles.progressCircle}>
