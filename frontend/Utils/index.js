0392c0196a67948d640a80e9fea2eb45c4dfb729
Olamide Tosin <larryTosn@gmail.com>
2022-04-11 06:56:46 -0400

    bug fixes

--- frontend/Utils/index.js
+++ frontend/Utils/index.js
@@ -1,5 +1,5 @@
 // const BASE_URL = 'http://localhost:3005/api/';  
-const BASE_URL = 'https://kwabaloanapi.herokuapp.com/api'; 
+const BASE_URL = 'https://kwabaloanapi.herokuapp.com/api/'; 
 export default BASE_URL;
 
 // Uncomment line 1 and comment line 2 for local deployment.
