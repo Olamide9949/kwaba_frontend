78531c3accef43bee27ec2bb90c3ef66edd7b662
Olamide Tosin <larryTosn@gmail.com>
2022-04-11 06:53:59 -0400

    progress bar update

--- frontend/component/ContentHeader/index.js
+++ frontend/component/ContentHeader/index.js
@@ -7,7 +7,7 @@ const ContentHeader = ({progressStatus, progressCompleted}) => {
         <div className={styles.contentHeader}>
             <h2>Payment Option</h2>
             <div className={styles.progressBar}>
-                <span> 1 of 3</span>
+                <span>{progressStatus ? (progressCompleted ? '3': '2') : '1'} of 3</span>
                 <div className={progressStatus ? (progressCompleted ? styles.progressCircleComplete: styles.progressCircle2) : styles.progressCircle}>
                     {progressCompleted && <Icon.BadgeCheckIcon width={25} height={25} color='#23dc9a'/>}
                 </div>
