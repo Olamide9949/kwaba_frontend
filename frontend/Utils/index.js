8d063cb7c10b479eb98ad86d63043117eced80b4
Olamide Tosin <larryTosn@gmail.com>
2022-04-11 16:09:23 -0400

    vercel linked

--- frontend/Utils/index.js
+++ frontend/Utils/index.js
@@ -1,5 +1,6 @@
 // const BASE_URL = 'http://localhost:3005/api/';  
-const BASE_URL = 'https://kwabaloanapi.herokuapp.com/api/'; 
+// const BASE_URL = 'https://kwabaloanapi.herokuapp.com/api/'; 
+const BASE_URL = 'https://loanbackend-8pfzx13vf-olamide9949.vercel.app/api/';
 export default BASE_URL;
 
-// Uncomment line 1 and comment line 2 for local deployment.
+// Uncomment line 1 and comment line 2 or 3 for local deployment.
